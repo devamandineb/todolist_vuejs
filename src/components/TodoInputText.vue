61d1c5c9011190d618b99163456a83766502a3ce
Amandine Carlier <dev-amandinecarlier@mailo.com>
2019-10-07 15:45:00 +0200

    add files

---
+++ src/components/TodoInputText.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    <input class="input is-rounded" type="text" placeholder="Rounded input">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>To-do list Disney!</h1>
+</template>
+
+<script>
+
+import TodoInputText from "@/components/TodoInputText.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
+
+export default {
+  name: "app",
+  components: {
+    TodoInputText
+  },
+
+data () {
+    return  [Mickey, Minnie, Porcinet];
+        }
+} ;
+</script>
+
+<style>
+
+</style>
